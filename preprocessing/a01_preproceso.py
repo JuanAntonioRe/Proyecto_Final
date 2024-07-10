65df03b16dfe9aeff28c82207a95fddf8b9eb62e
juan antonio <juan_ryes.94@hotmail.com>
2024-07-09 21:05:13 -0600

    Continuacion preproceso

--- preprocessing/a01_preproceso.py
+++ preprocessing/a01_preproceso.py
@@ -5,6 +5,9 @@ sys.path.append(os.getcwd)
 
 # Importación de los datasets
 contract_df = pd.read_csv('datasets/contract.csv')
+personal_df = pd.read_csv('datasets/personal.csv')
+internet_df = pd.read_csv('datasets/internet.csv')
+phone_df = pd.read_csv('datasets/phone.csv')
 
 def limpiar_columna(data):
     # Cambia las celdas que tienen ' ' por NaN
@@ -16,5 +19,25 @@ def limpiar_columna(data):
     data['TotalCharges'].fillna(promedio, inplace=True)
     return data
 
+# Función que coloca cero o 1 en la nueva columna si se ha terminado el contrato
+def ContractEnding(end_date):
+    if end_date == 'No':
+        return 0
+    else:
+        return 1
+    
 contract_df = limpiar_columna(contract_df)
-print(contract_df.head(5))
+contract_df['ContractEnd'] = contract_df['EndDate'].apply(ContractEnding)
+
+# Juntando todos los datasets en uno solo
+merge_df = contract_df.merge(personal_df, on='customerID', how='left').merge(internet_df, on='customerID', how='left').merge(phone_df, on='customerID', how='left')
+
+# Checando valores NaN
+merge_df.info()
+# Se quitan los valores NaN
+merge_df = merge_df.fillna('None')
+merge_df.info()
+
+
+
+#print(merge_df.head(5))
